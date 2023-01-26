4beecb673535d3cc5e7f9f057cb85425496e8bb6
Alejandro Lara Gutiérrez <laragutz@gmail.com>
2023-01-26 14:51:39 -0600

    agregando detalles

--- scripts/code_newpost.js
+++ scripts/code_newpost.js
@@ -34,18 +34,64 @@ const previewButton = () =>{
 };
 
 const extractData = () =>{
-    const inputsList = document.querySelectorAll('#formPostNew input');
+    const inputsList = document.querySelectorAll('#formPostNew input[type="text"],input[type="url"], textarea');
     const dataPost = {};
     
     inputsList.forEach((input) => {
         dataPost[input.name] = input.value;
     });
-
+    drawPreview(dataPost);
 };
 
-const drawPreview = (dataPost) =>{
-
+const drawPreview = (data) =>{
+    clearForm();
+    const indexPost = document.querySelector('#formPostNew');
+    const articlePost = document.createElement('article');
+    articlePost.classList.add('articleNew');
+    indexPost.appendChild(articlePost);
+    const headerArticle = document.createElement('header');
+    articlePost.appendChild(headerArticle);
+    const divHeader = document.createElement('div');
+    headerArticle.appendChild(divHeader);
+    const h1Div = document.createElement('h1');
+    h1Div.textContent = data.title;
+    divHeader.appendChild(h1Div);
+    const divSymbols = document.createElement('div');
+    headerArticle.appendChild(divSymbols);
+    const h4Symbols = document.createElement('h4');
+    h4Symbols.textContent = data.symbols;
+    divSymbols.appendChild(h4Symbols);
+    const divArticle = document.createElement('div');
+    articlePost.appendChild(divArticle);
+    const pName = document.createElement('p');
+    pName.textContent = data.name;
+    divArticle.appendChild(pName);
+    
 };
 
+const clearForm = () =>{
+    const removeElement = document.querySelectorAll('div.formPost');
+    removeElement.forEach((element) => {
+        element.remove();
+    });
+};
+
+const editButton = () =>{
+    const formPreview = document.querySelector('#btnEdit');
+    formPreview.addEventListener('click',() => {
+        fillData();
+    });
+};
+
+const fillData = () =>{
+    alert('fillData');
+    clearPreview();
+};
+
+
+const clearPreview = () =>{
+    alert('clearPreview');
+};
 
 previewButton();
+editButton();
